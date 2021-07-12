f7f5cb0749f0d140942d71f24d59f8984c9f65ce
Ritul Patel <49111661+codeonec@users.noreply.github.com>
2021-07-12 15:31:15 +0530

    Update keys.js

--- control-center/config/keys.js
+++ control-center/config/keys.js
@@ -1,5 +1,5 @@
 const MongoURI = 'mongodb+srv://testnode:test1234@cluster0.qz4ys.mongodb.net/🚀?retryWrites=true&w=majority'
-// MainSTR: y0C0&trol@[project].8*#0^asu
+
 const ControlCenterPass = '$2a$10$4cpxMBFx0e6opR/IBBK.Pua796wbtz7iH9OYKFC1NoSnXLD2/n4gK'
 const TokenSecret = 'A@6sqbv%ashd#nasbQYZBA&8aehw(*sa&jasn)jasn(asneh$##WSA412as)saw'
 
