1f41fe799122b407fa3da65a294a5f0d07ee9377
Keval <skeval904@gmail.com>
2022-01-30 13:17:43 +0530

    starting UPrise

--- control-center/config/keys.js
+++
@@ -1,8 +0,0 @@
-const MongoURI = 'mongodb+srv://testnode:test1234@cluster0.qz4ys.mongodb.net/🚀?retryWrites=true&w=majority'
-
-const ControlCenterPass = '$2a$10$4cpxMBFx0e6opR/IBBK.Pua796wbtz7iH9OYKFC1NoSnXLD2/n4gK'
-const TokenSecret = 'A@6sqbv%ashd#nasbQYZBA&8aehw(*sa&jasn)jasn(asneh$##WSA412as)saw'
-
-module.exports.MongoURI = MongoURI
-module.exports.ControlCenterPass = ControlCenterPass
-module.exports.TokenSecret = TokenSecret
