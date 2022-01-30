39808ed76762ea76b1ab8baa3b3dee18e6fe91b3
Rushil45 <srushil63@gmail.com>
2022-01-30 15:54:32 +0530

    commit

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">UPRised!</a>
+          Welcome to <a href="https://nextjs.org">UPRise!</a>
         </h1>
 
         <p className={styles.description}>
