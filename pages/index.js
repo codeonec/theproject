b7de16764275926b7e97c2021c43036a882f4246
Keval <skeval904@gmail.com>
2022-02-15 20:34:22 +0530

    added meta tags

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,13 @@ export default function Home() {
       <Head>
         <title>Uprise</title>
         <meta name="description" content="UPRISE.COM" />
+        <meta property="theme-color" content="#000000" />
+        <meta property="og:site_name" content="Uprise.com" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Uprise. Funding next big thing" />
+        <meta property="og:type" content="website" />
+        {/* <meta property="og:image" itemprop="image" content="./"/> */}
+        <meta property="og:description" content="invest in startups from all segments or participate by being an early employee" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
     </div>
