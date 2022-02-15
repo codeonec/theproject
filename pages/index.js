beddf3db7d3a00f1e31799a9e827966e2e3fb96f
Ritul Patel <49111661+codeonec@users.noreply.github.com>
2022-02-15 20:00:40 +0530

    Fuck Yeah

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">UPRise!</a>
+          Welcome to <a href="https://nextjs.org">UPRise! Fuck Yeah</a>
         </h1>
 
         <p className={styles.description}>
