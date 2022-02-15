61e52adfede754f24996d80927c6c25f40613d5b
Darshan Suthar <darhsansu156@gmail.com>
2022-02-15 20:05:10 +0530

    added head

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      
+      <Head>
+        <title>Uprise</title>
+        <meta name="description" content="UPRISE.COM" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
     </div>
-  )
+  );
 }
