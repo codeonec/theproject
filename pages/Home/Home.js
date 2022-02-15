c791017f50e0c92f75b229aa3bd0a989b4a089fd
Darshan Suthar <darhsansu156@gmail.com>
2022-02-15 21:11:16 +0530

    created files for components and pages

---
+++ pages/Home/Home.js
@@ -0,0 +1,11 @@
+import Feed from "./Feed/Feed";
+import "./Home.css";
+const Home = () => {
+  return (
+    <div>
+      <h1>HOME</h1>
+      <Feed />
+    </div>
+  );
+};
+export default Home;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Uprise</title>
         <meta name="description" content="UPRISE.COM" />
@@ -14,10 +13,14 @@ export default function Home() {
         <meta property="og:title" content="Uprise. Funding next big thing" />
         <meta property="og:type" content="website" />
         {/* <meta property="og:image" itemprop="image" content="./"/> */}
-        <meta property="og:description" content="invest in startups from all segments or participate by being an early employee" />
+        <meta
+          property="og:description"
+          content="invest in startups from all segments or participate by being an early employee"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>Hello</h1>
+      <Home />
     </div>
   );
 }
