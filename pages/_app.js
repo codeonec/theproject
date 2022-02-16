b2282287a1a5f92c037ef20ff337a268e429dc81
Ritul Patel <49111661+codeonec@users.noreply.github.com>
2022-02-16 20:33:37 +0530

    added conditions

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import '../styles/globals.css'
 import SideBar from '../components/SideBar'
 import TopBar from '../components/TopBar'
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
   return (
     <div className='app_container'>
-      <SideBar />
-      <TopBar />
+      {router.pathname !== '/sign_in' && <SideBar />}
+      {router.pathname !== '/sign_in' && <TopBar />}
       <Component {...pageProps} />
     </div>
   )
