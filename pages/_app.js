adb96bb2df7480ad4e4d8852597cd433bb0154e1
Keval <skeval904@gmail.com>
2022-02-17 12:08:21 +0530

    JS badli according to css modules🔥

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <div>
       <Head>
-        <link rel="icon" href="/favicon.svg" />
+        <link rel="icon" href="icons/favicon.svg" />
       </Head>
       <style jsx>
           {`
--- pages/sign_in.js
+++ pages/sign_in.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import Button from "../components/Button";
+import styles from "../styles/sign_in.module.css";
 
 export default function Home() {
   return (
@@ -19,13 +20,13 @@ export default function Home() {
           content="invest in startups from all segments or participate by being an early employee"
         />
       </Head>
-      <div className="logo">
+      <div className={styles.logo}>
         <img src="icons/logo.svg" alt="" />
       </div>
-      <div className="loginInterface">
-        <div className="banner">Log in to Uprise</div>
-        <div className="sso">
-          <div className="ssoLogin">
+      <div className={styles.loginInterface}>
+        <div className={styles.banner}>Log in to Uprise</div>
+        <div className={styles.sso}>
+          <div className={styles.ssoLogin}>
             <img src="icons/google.svg" alt="" />
             <p>Continue with Google</p>
           </div>
@@ -37,30 +38,30 @@ export default function Home() {
               icon: <img src="/google.svg" alt="" />,
             }}
           /> */}
-          <div className="ssoLogin">
+          <div className={styles.ssoLogin}>
             <img src="icons/apple.svg" alt="" />
             <p>Continue with Apple</p>
           </div>
         </div>
-        <p className="or">OR</p>
-        <div className="normalLogin">
-          <div className="Email field">
+        <p className={styles.or}>OR</p>
+        <div className={styles.normalLogin}>
+          <div className={styles.field}>
             <p>Email or Username</p>
             <input type="text" placeholder="Email or Username" />
           </div>
-          <div className="Password field">
+          <div className={styles.field}>
             <p>Password</p>
             <input type="text" placeholder="Password" />
-          </div>
-          <div className="loginButton">Log in</div>
-          <div className="signup">
-            Don&apos;t have an account?{" "}
+      </div>
+          <div className={styles.loginButton}>Log in</div>
+          <div className={styles.signup}>
+            Don&apos;t have an account?
             <span>
               <a href="/sign_up">Sign up</a>
             </span>
           </div>
         </div>
-      </div>
-    </div>
+        </div>
+        </div>
   );
 }
