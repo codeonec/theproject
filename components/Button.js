436a35d3c255ca88e3702c76eeb2166253956f1d
Keval <skeval904@gmail.com>
2022-02-19 18:01:35 +0530

    some problem in button.js

--- components/Button.js
+++ components/Button.js
@@ -1,36 +1,11 @@
-import React from "react";
+import styles from "../styles/Button.module.css"
+
 function Button({ specs }) {
   return (
     <div>
-      <style jsx>
-        {`
-          .btn {
-            display: flex;
-            align-items: center;
-
-            // height: ${specs.height}rem;
-            line-height: 0px;
-            // width: ${specs.width}rem;
-            padding: 0.2rem 1.5rem;
-            background-color: var(--color-brand);
-            color: var(--text-main);
-            gap: 5px;
-            border-radius: 30px;
-            border: none;
-            font-size: 1rem;
-            cursor: pointer;
-          }
-          .btn:hover {
-            background-color: #1a682a;
-          }
-          .btn:focus {
-            border: none;
-          }
-        `}
-      </style>
-      <button className="btn">
+      <button className={styles.btn}>
         {specs.icon && specs.icon}
-        <p className="btn_text">{specs.text}</p>
+        <p className={styles.button_text}>{specs.text}</p>
       </button>
     </div>
   );
--- components/Card.js
+++ components/Card.js
@@ -1,24 +1,54 @@
 import styles from "../styles/Card.module.css";
+import Button from "../components/Button";
 
 export default function Card() {
   return (
     <div className={styles.Card}>
       <div className={styles.information}>
         <div className={styles.profile}>
-          <div className={styles.pic}>
+          <div id="logo" className={styles.pic}>
             <img src="pngs/icon.png" alt="" />
           </div>
           <div className={styles.title}>
-            Notion <br />
-            <span>Founded in 2010 by Notion Inc.</span>
+            <div id="companyName">Notion</div>
+            <span id="foundedBy">Founded in 2010 by Notion Inc.</span>
           </div>
         </div>
         <div className={styles.description}>
-          
+          The PEER fund thesis: a crypto-focused venture fund will outperform
+          traditional venture capital over the next 10 years. We are investing
+          in pre-seed, seed, series A with follow-on and focused on equity and
+          token.
+        </div>
+        <div className={styles.likeNshare}>
+          <img src="Icons/like.svg" alt="" />
+          <span id="likeCount">69K</span>
+          <img src="Icons/share.svg" alt="" />
+          <span id="shareCount">Share</span>
         </div>
       </div>
       <div className={styles.stats}>
-
+        <div className={styles.topStats}>
+          <div className={styles.statTitle}>
+            <div>Valuation</div> <br /> <span id="valuation">$69 Bn.</span>
+          </div>
+          <div className={styles.statTitle + " " + styles.revenue}>
+            <div>Revenue</div> <br /> <span id="Revenue">$6 Mn.</span>
+          </div>
+        </div>
+        <div className={styles.statTitle + " " + styles.price}>
+          <div>Price</div> <br />{" "}
+          <div className={styles.priceWrap}>
+            <span id="Price">
+              $982.00
+                <img src="Icons/uparrow.svg" alt="" />
+                <span className={styles.percent}>6.12%</span>
+            </span>
+          </div>
+        </div>
+        <div className={styles.invest}>
+          <Button></Button>
+        </div>
       </div>
     </div>
   );
