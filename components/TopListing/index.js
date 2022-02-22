80c02425b256fca650db42ece7a2a280ca6240c9
Darshan Suthar <darhsansu156@gmail.com>
2022-02-22 17:10:15 +0530

    notin much

--- components/TopListing/index.js
+++ components/TopListing/index.js
@@ -2,11 +2,9 @@ import styles from "../../styles/TopListing.module.css";
 
 export default function TopListing() {
   return (
-      <div className={styles.container}>
-        <div class={styles.title}>New Listings</div>
-        <div className={styles.element}>
-            india
-        </div>
-      </div>
+    <div className={styles.container}>
+      <div className={styles.title}>New Listings</div>
+      <div className={styles.element}>india</div>
+    </div>
   );
 }
